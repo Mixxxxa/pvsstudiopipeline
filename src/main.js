0515177dc4b716db38b8e0c88cbdae7789561471
Mikhail Gelvikh <gelvihmihail@gmail.com>
2024-07-04 13:23:22 +0300

    #1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 const core = require('@actions/core')
+const exec = require('@actions/exec')
+
 const { wait } = require('./wait')
+
+import { platform } from '@actions/core'
 
 /**
  * The main function for the action.
@@ -7,15 +11,18 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    core.debug('RUN')
+    await installAnalyzer()
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // const ms = core.getInput('milliseconds', { required: true })
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    // core.debug(`Waiting ${ms} milliseconds ...`)
+
+    // // Log the current timestamp, wait, then log the new timestamp
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
@@ -25,6 +32,33 @@ async function run() {
   }
 }
 
+async function installAnalyzer() {
+  try {
+    core.debug('Trying to install analyzer')
+    if (platform.isWindows) {
+      core.debug('Detected Windows')
+      await exec.exec('choco', ['install', 'pvs-studio'])
+    } else if (platform.isLinux) {
+      core.debug('Detected Linux')
+      await exec.exec('sudo', ['apt-get', 'update'])
+      await exec.exec('sudo', ['apt-get', 'install', 'pvs-studio'])
+    } else if (platform.isMacOS) {
+      core.debug('Detected macos')
+      await exec.exec('brew update')
+      await exec.exec('brew', ['install', 'viva64/pvs-studio/pvs-studio'])
+    } else {
+      throw new Error('Unsuppoted OS')
+    }
+    core.debug(`Successfuly installed PVS-Studio `)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+async function getAnalyzerVersion() {}
+
 module.exports = {
-  run
+  run,
+  installAnalyzer,
+  getAnalyzerVersion
 }
