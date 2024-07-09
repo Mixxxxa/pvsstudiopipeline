de80d057233b656c70a2dcb9006e78aa956a25a7
Mikhail Gelvikh <gelvihmihail@gmail.com>
2024-07-10 00:48:57 +0300

    Checkpoint

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,289 @@ import * as fsp from 'node:fs/promises'
 import * as os from 'node:os'
 import * as path from 'node:path'
 
-//const { wait } = require('./wait')
-//import { platform } from '@actions/core'
+namespace PVSStudio {
+  export namespace Errors {
+    export class PVSError extends Error {
+      constructor(message: string) {
+        super(message);
+        this.name = "PVSError";
+      }
+    }
+
+    export class Unimplemented extends PVSError {
+      constructor() {
+        super('Unimplemented');
+        this.name = "Unimplemented";
+      }
+    }
+
+    export class AnalyzerNotFound extends PVSError {
+      constructor(message: string) {
+        super(message);
+        this.name = "AnalyzerNotFound";
+      }
+    }
+
+    export class UnsuppotedPlatform extends PVSError {
+      constructor() {
+        super('Unsupported platfrom');
+        this.name = "UnsuppotedPlatform";
+      }
+    }
+  }
+
+  export namespace Utils {
+    export function splitStringValues(text: string): Array<string> {
+      if (text.length === 0) {
+        return [];
+      }
+
+      const regex = /[;\n]/
+      const parts = text.split(regex)
+      const filteredParts = parts.filter(part => part.trim() !== '')
+      return filteredParts;
+    }
+
+    export function appendArgs(container: Array<string>, values: Array<string>, flag?: string) : void {
+      for (let value of values) {
+        if(flag){
+          container.push(flag)
+        }
+        container.push(value)
+      }
+    }
+
+    export const OptionalInputWithTrim: core.InputOptions = {
+      required: false,
+      trimWhitespace: true
+    }
+  
+    export const RequiredInputWithTrim: core.InputOptions = {
+      required: false,
+      trimWhitespace: true
+    }
+
+    export function getBackend() : Backend.AbstractPlatformBackend {
+      if (process.platform === 'win32') {
+        core.debug('Detected Windows')
+        return new Backend.Windows();
+      } else if (process.platform === 'darwin') {
+        core.debug('Detected macOS')
+        throw new Errors.UnsuppotedPlatform();
+      } else if (process.platform === 'linux') {
+        core.debug('Detected Linux')
+        return new Backend.Linux();
+      } 
+      throw new Errors.UnsuppotedPlatform();
+    }
+  }
+
+  namespace Backend {
+    export abstract class AbstractPlatformBackend {
+
+      public async createFile(filepath: string, content: string): Promise<void> {
+        return fsp.writeFile(filepath, content)
+      }
+
+      public async createTempFile(content: string, fileExt?: string): Promise<string> {
+        let tmpFileName = 'pvs-';
+        if(fileExt)
+        {
+          tmpFileName += fileExt;
+        }
+        const tmpFilePath = path.join(os.tmpdir(), tmpFileName);
+        await this.createFile(tmpFilePath, content);
+        return tmpFilePath;
+      }
+      
+      // public abstract installPath(): string;
+
+      // public abstract plogConverterFilePath(): string;
+
+      public async exportLicenseFromEnvVars() : Promise<string> {
+        const name = process.env.PVS_STUDIO_LICENSE_NAME
+        const key = process.env.PVS_STUDIO_LICENSE_KEY
+        let tempLicenseFilePath = ''
+        if (name && key) {
+          const licenseFileContent = `${name}\n${key}`;
+          tempLicenseFilePath = await this.createTempFile(licenseFileContent);
+        }
+        return tempLicenseFilePath;
+      }
+    };
+
+    export class Windows extends AbstractPlatformBackend {
+
+      public async install(_: string) : Promise<void> {
+        core.info('Installing PVS-Studio analyzer on Windows via choco');
+        const res = await exec.exec('choco', ['install', 'pvs-studio']);
+        if(res !== 0) {
+          throw new PVSStudio.Errors.PVSError(`Unable to install analyzer. Details: ${res}`);
+        }
+        core.debug('PVS-Studio successfuly installed');
+      }
+    };
+
+    export class Linux extends AbstractPlatformBackend {
+
+      public async install(analyzer: string) : Promise<void> {
+        core.info(`Installing PVS-Studio (${analyzer}) on Linux via direct download`)
+        let downloadLink = ''
+        switch(analyzer){
+          case 'cpp':
+            downloadLink = 'https://cdn.pvs-studio.com/pvs-studio-latest.deb'; 
+            break;
+          case 'csharp':
+            // There are no *latest links
+            throw new PVSStudio.Errors.Unimplemented();
+            break;
+          case 'java':
+            downloadLink = 'https://cdn.pvs-studio.com/pvs-studio-java.zip';
+            break;
+          default:
+            throw new PVSStudio.Errors.PVSError(`Tried to install unknown analyzer '${analyzer}'`)
+        }
+
+        const distFilePath: string = await tc.downloadTool(downloadLink);
+        if (analyzer in ['cpp', 'csharp']) {
+          const newDistFilePath = `${distFilePath}.deb`
+          await io.mv(distFilePath, newDistFilePath)
+          await exec.exec('sudo', [
+            'apt-get',
+            'install',
+            `${core.toPlatformPath(newDistFilePath)}`
+          ])
+        } else if(analyzer === 'java') {
+          throw new PVSStudio.Errors.Unimplemented();
+        }
+        core.debug('PVS-Studio successfuly installed');
+      }
+    };
+
+    export class macOS extends AbstractPlatformBackend {
+
+      public async install(analyzer: string) : Promise<void> {
+        core.info(`Installing PVS-Studio (${analyzer}) on macOS via brew`)
+        await exec.exec('brew update')
+        await exec.exec('brew', ['install', 'viva64/pvs-studio/pvs-studio'])
+        if(analyzer === 'csharp'){
+          await exec.exec('brew', ['install', 'viva64/pvs-studio/pvs-studio-dotnet'])
+        } else if(analyzer === 'java') {
+          throw new PVSStudio.Errors.Unimplemented();
+        }
+        core.debug('PVS-Studio successfuly installed');
+      }
+    };
+  }
+
+  export namespace Analyzer {
+    abstract class AbstractAnalyzer {
+      protected backend: Backend.AbstractPlatformBackend;
+    
+      constructor(backend: Backend.AbstractPlatformBackend){
+        this.backend = backend;
+      }
+    
+      public abstract run() : Promise<string>;
+    };
+
+    enum CppAnalyzerCLIFlags {
+      AnalyzeModeKey = "analyze",
+      InputFile = '-f',
+      OutputFile = "-o",
+      AnalysisMode = "-a",
+      ExcludedDir = "-e",
+      LicenseFile = "-l",
+      SuppressFile = "-s",
+      RulesConfigFile = "-R",
+      ParallelCount = "-j"
+    };
+    
+    export class CppAnalyzer extends AbstractAnalyzer {
+    
+      public coreFilePath() : string {
+    
+      }
+    
+      public analyzerFilePath() : string {
+
+    
+      }
+    
+      public available() : boolean {
+        
+      }
+    
+      public async install() : Promise<void> {
+        return this.backend.
+      }
+
+      protected generateRawLogFilePath(sourceFilePath: string) : string {
+        const parts = path.parse(sourceFilePath)
+        return `${parts.dir}/${parts.name}-raw.log`
+      }
+    
+      protected async createArgs() : Promise<Array<string>> {
+        const Flags = CppAnalyzerCLIFlags;
+        let args = [
+          Flags.AnalyzeModeKey,
+          Flags.InputFile, core.getInput('file-to-analyze', Utils.RequiredInputWithTrim),
+          Flags.AnalysisMode, core.getInput('analysis-mode', Utils.OptionalInputWithTrim)
+        ]
+
+        const outputText = core.getInput('output-file', Utils.RequiredInputWithTrim);
+        args.push(Flags.InputFile, this.generateRawLogFilePath(outputText));
+
+        const excludesText = core.getInput('excluded-dirs', Utils.OptionalInputWithTrim)
+        Utils.appendArgs(args, Utils.splitStringValues(excludesText), Flags.ExcludedDir);
+        
+        const suppressText = core.getInput('suppress-files', Utils.OptionalInputWithTrim)
+        Utils.appendArgs(args, Utils.splitStringValues(suppressText), Flags.SuppressFile);
+
+        const rulesConfigsText = core.getInput('rules-configs', Utils.OptionalInputWithTrim)
+        Utils.appendArgs(args, Utils.splitStringValues(rulesConfigsText), Flags.RulesConfigFile);
+
+        const parallelText = core.getInput('parallel', Utils.OptionalInputWithTrim)
+        if (parallelText && parallelText !== '0') {
+          args.push(Flags.ParallelCount, parallelText)
+        }
+
+        const licenseFileText = core.getInput('licence-file', Utils.OptionalInputWithTrim);
+        args.push(Flags.LicenseFile);
+        if(licenseFileText) {
+          args.push(licenseFileText)
+        } else {
+          const tempLicenseFilePath = await this.backend.exportLicenseFromEnvVars();
+          args.push(tempLicenseFilePath)
+        }
+
+        return args;
+      }
+    
+      public async run() : Promise<string> {
+        const args = await this.createArgs();
+        const analyzerExecutable = await this.analyzerFilePath();
+        const res = await exec.getExecOutput(`"${analyzerExecutable}"`, args)
+        if(res.exitCode !== 0) {
+          throw new Error(
+            `Analyzer exited with code ${res.exitCode}. Details: ${res}`
+          )
+        }
+
+      }
+    };
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
 /**
  * The main function for the action.
@@ -16,9 +297,14 @@ import * as path from 'node:path'
 export async function run(): Promise<void> {
   try {
     core.debug('RUN')
-    await installAnalyzer()
-    await runAnalyzer()
-    await convertReport()
+    const backend = PVSStudio.Utils.getBackend();
+    const analyzer = new PVSStudio.Analyzer.CppAnalyzer(backend);
+    if(!analyzer.available()) {
+      analyzer.install();
+    }
+
+    const rawReportFilePath = await analyzer.run();
+    core.setOutput('raw-report', rawReportFilePath);
     core.debug('FINISH')
   } catch (error: any) {
     core.setFailed(error.message)
